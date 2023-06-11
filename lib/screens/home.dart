3b3b74a26d6890c86ac50b441902cb9535c049e9
أمان الله منجلي <menjliamanullah@gmail.com>
2023-06-11 12:34:39 +0000

    finished copying code from a tutorial to try it

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation colorAnimation;
+  late Animation sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    colorAnimation = ColorTween(begin: Colors.blue, end: Colors.yellow)
+      .animate(CurvedAnimation(parent: controller, curve: Curves.bounceOut));
+    sizeAnimation = Tween<double>(begin: 100.0, end: 200.0).animate(controller);
+
+    // In initState
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Animation Demo"),
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+        title: const Text("Animation Demo"),
+      ),
+      body: Center(
+        child: Container(
+          height: sizeAnimation.value,
+          width: sizeAnimation.value,
+          color: colorAnimation.value,
+        ),
       ),
     );
   }
